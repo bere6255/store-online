6e69b9b2fe669fd1fa6dfb996c6f1fecfe382dfd
paul bereobong clement <clement.bere@yahoo.com>
2020-10-26 15:23:40 +0100

    remove token

--- src/api/axios/yelp.js
+++ src/api/axios/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const yelpAPI = axios.create({
     baseURL: 'https://api.yelp.com/v3',
     timeout: 1000,
-    headers: { Authorization: 'Bearer 3dEfcXK3h2w4Macpyg_7HEeYKRtI1CCBADHV9c4js2sdcOIJgstegV-joi775JwoS6KkJZLWUNhh8S4Wea_mSP6CIhPeFjnWQEUZOL-ilAqMfeqF2UDhdtRyiR6GX3Yx' }
+    headers: { Authorization: 'Bearer ' }
 });
 
 export default yelpAPI;
